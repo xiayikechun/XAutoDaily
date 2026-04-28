6de04ef29f5c060f8fec09caeec9f5b218b1d75c
hd <hdshare@vip.qq.com>
2026-04-28 11:25:44 +0800

    修复亲密空间方法在旧版本异常的问题

--- app/src/main/java/me/teble/xposed/autodaily/hook/function/impl/QZIntimateSpaceManager.kt
+++ app/src/main/java/me/teble/xposed/autodaily/hook/function/impl/QZIntimateSpaceManager.kt
@@ -1,26 +1,38 @@
 package me.teble.xposed.autodaily.hook.function.impl
 
-import com.github.kyuubiran.ezxhelper.utils.invokeMethod
+import com.github.kyuubiran.ezxhelper.utils.findMethod
 import me.teble.xposed.autodaily.hook.base.load
 import me.teble.xposed.autodaily.hook.function.base.BaseFunction
 import me.teble.xposed.autodaily.hook.utils.QApplicationUtil
 import me.teble.xposed.autodaily.utils.new
 import mqq.app.AppRuntime
+import java.lang.reflect.Method
 
 open class QZIntimateSpaceManager : BaseFunction(
     TAG = "QZIntimateSpaceManager"
 ) {
     lateinit var viewModel: Any
+    lateinit var checkInMethod: Method
 
     override fun init() {
         val viewModelClz = load("Lcom/qzone/reborn/intimate/viewmodel/QZIntimateCheckInViewModel;")!!
         viewModel = viewModelClz.new()
+        checkInMethod = findMethod(viewModelClz) {
+            val types = parameterTypes
+            if (types.size != 2) return@findMethod false
+            val oldParam = types[0] == String::class.java && types[1] == AppRuntime::class.java // 9.2.30
+            val newParam = types[0] == AppRuntime::class.java && types[1] == String::class.java // 9.2.80
+            oldParam || newParam
+        }
     }
 
     open fun doCheckInRequest(spaceId: String) {
-        viewModel.invokeMethod(QApplicationUtil.appRuntime, spaceId) {
-            val paramTypes = parameterTypes
-            paramTypes.size == 2 && paramTypes[0] == AppRuntime::class.java && paramTypes[1] == String::class.java
+        checkInMethod.also { m ->
+            if (m.parameterTypes[0] == String::class.java) {
+                m.invoke(viewModel, spaceId, QApplicationUtil.appRuntime)
+            } else {
+                m.invoke(viewModel, QApplicationUtil.appRuntime, spaceId)
+            }
         }
     }
 }
